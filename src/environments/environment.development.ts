a79c40e5a810141f6f4bb3c6befbdf8d601c3aa3
shady <shady@gmail.com>
2025-05-06 03:04:59 +0300

    edit enviroonment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,5 @@
 export const environment = {
-  // baseUrl: 'https://note-sigma-black.vercel.app/api/v1/users/',
-  // notesUrl: 'https://note-sigma-black.vercel.app/api/v1/',
-  notesUrl: 'http://localhost:3000/api/notes',
-  registerURL: 'http://localhost:3000/api/auth/register',
-  loginURL: 'http://localhost:3000/api/auth/login',
+  notesUrl: 'http://backend:3000/api/notes',
+  registerURL: 'http://backend:3000/api/auth/register',
+  loginURL: 'http://backend:3000/api/auth/login',
 };
